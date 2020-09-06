ae02d0eacaaab0e3f0662cbb9cc5847f21985afd
Roman Kalachik <RomanKalachik@gmail.com>
2020-09-06 15:27:56 +0300

    first commit

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,37 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.Threading;
+using System;
+
+namespace TestUI {
+    public class MainWindow : Window {
+
+
+        public static readonly AvaloniaProperty<int> PropertyDeclaration =
+     AvaloniaProperty.Register<MainWindow, int>("Angle", inherits: true);
+
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent() {
+            AvaloniaXamlLoader.Load(this);
+            DataContext=this;
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval=TimeSpan.FromMilliseconds(30);
+            timer.Tick+=Timer_Tick;
+            Angle=0;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e) {
+            Angle+=1;
+        }
+
+        public int Angle {
+            get { return (int)this.GetValue(PropertyDeclaration); }
+            set { this.SetValue(PropertyDeclaration, value); }
+        }
+    }
+}
